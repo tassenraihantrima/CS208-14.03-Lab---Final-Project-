4e0797e9c6d3fe7c6429500d1fc18f93c1c9646b
Tassen Raihan Trima <84325319+tassenraihantrima@users.noreply.github.com>
2025-12-03 22:39:11 +0000

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next){
-  try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
-        console.error('Error fetching todos:', err);
-        return res.status(500).send('Error fetching todos');
-      }
-      res.render('index', { title: 'My Simple TODO', todos: results });
-    });
-  } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
+let comments = [
+  {
+    name: 'Sam',
+    message: 'Best maple bar in downtown Boise!',
+    date: '11/30/2025'
   }
+];
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Downtown Donuts' });
 });
 
-router.post('/create', function (req, res, next) {
-    const { task } = req.body;
-    try {
-      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => {
-        if (err) {
-          console.error('Error adding todo:', err);
-          return res.status(500).send('Error adding todo');
-        }
-        console.log('Todo added successfully:', results);
-        // Redirect to the home page after adding
-        res.redirect('/');
-      });
-    } catch (error) {
-      console.error('Error adding todo:', error);
-      res.status(500).send('Error adding todo');
-    }
+/* GET menu page. */
+router.get('/menu', function (req, res, next) {
+  res.render('menu', { title: 'Menu - Downtown Donuts' });
 });
 
-router.post('/delete', function (req, res, next) {
-    const { id } = req.body;
-    try {
-      req.db.query('DELETE FROM todos WHERE id = ?;', [id], (err, results) => {
-        if (err) {
-          console.error('Error deleting todo:', err);
-          return res.status(500).send('Error deleting todo');
-        }
-        console.log('Todo deleted successfully:', results);
-        // Redirect to the home page after deletion
-        res.redirect('/');
+/* GET about page. */
+router.get('/about', function (req, res, next) {
+  res.render('about', { title: 'About Us - Downtown Donuts' });
+});
+
+/* GET comments page. */
+router.get('/comments', function (req, res, next) {
+  res.render('comments', {
+    title: 'Comments - Downtown Donuts',
+    comments
+  });
+});
+
+/* POST new comment. */
+router.post('/comments', function (req, res, next) {
+  const { name, message } = req.body;
+
+  if (name && message) {
+    comments.unshift({
+      name,
+      message,
+      date: new Date().toLocaleDateString()
     });
-    }catch (error) {
-        console.error('Error deleting todo:', error);
-        res.status(500).send('Error deleting todo:');
-    }
+  }
+
+  res.redirect('/comments');
 });
 
 module.exports = router;
